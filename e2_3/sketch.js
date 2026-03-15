0272579090ca53b879d0edbf54e8a24b2f8271e3
Carda01 <1000021334@studium.unict.it>
2026-03-15 17:19:25 +0100

    ex2.3 done

---
+++ e2_3/sketch.js
@@ -0,0 +1,69 @@
+class Mover {
+  constructor(x, y, mass) {
+    this.mass = mass;
+    this.position = createVector(x, y, mass);
+    this.velocity = createVector(0, 0);
+    this.acceleration = createVector(0, 0);
+  }
+
+  applyForce(force) {
+    let f = p5.Vector.div(force, this.mass);
+    this.acceleration.add(f);
+  }
+
+  update() {
+    let edgeForce = createVector(map(this.position.x, 0, width, 0, 0.1) - 0.05, map(this.position.y, 0, height, 0, 0.1) - 0.05).mult(-1);
+    this.applyForce(edgeForce);
+    this.velocity.add(this.acceleration);
+    this.position.add(this.velocity);
+
+    this.acceleration.mult(0);
+  }
+
+  show() {
+    stroke(0);
+    fill(175);
+
+    circle(this.position.x, this.position.y, this.mass * 16);
+  }
+
+  checkEdges() {
+    if (this.position.x > width - (this.mass * 8)) {
+      this.position.x = width - this.mass * 8;
+      this.velocity.x *= -1;
+    } else if (this.position.x < (this.mass * 8)) {
+      this.velocity.x *= -1;
+      this.position.x = this.mass * 8;
+    }
+
+    if (this.position.y > height - (this.mass * 8)) {
+      this.velocity.y *= -1;
+      this.position.y = height - this.mass * 8;
+    }
+  }
+}
+
+let movers;
+
+function setup() {
+  createCanvas(600, 600);
+  movers = new Array(10);
+  for (let i = 0; i < 10; i++) {
+    movers[i] = new Mover(random(width), random(height), random(10))
+  }
+}
+
+function draw() {
+  background(255);
+  let gravity = createVector(0, 0.1);
+  let wind = createVector(0.05, 0);
+  for (let i = 0; i < 10; i++) {
+    if (mouseIsPressed) {
+      movers[i].applyForce(wind);
+    }
+    movers[i].applyForce(gravity);
+    movers[i].checkEdges();
+    movers[i].update();
+    movers[i].show();
+  }
+}
